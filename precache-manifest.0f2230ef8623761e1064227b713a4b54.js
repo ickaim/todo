4f0e7f53516c7262ba8f171e3a019033335d44be
Ivana Markovic <ickaim@gmail.com>
2020-01-22 20:20:41 +0100

    Updates

---
+++ precache-manifest.0f2230ef8623761e1064227b713a4b54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0e6f39ae85abb55dd3aad81415b442",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "99c7f02ecbb5aea3d815",
+    "url": "/todo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c570cf718a0498076c29",
+    "url": "/todo/static/css/main.a14a2d09.chunk.css"
+  },
+  {
+    "revision": "99c7f02ecbb5aea3d815",
+    "url": "/todo/static/js/2.3333a49f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/todo/static/js/2.3333a49f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c570cf718a0498076c29",
+    "url": "/todo/static/js/main.e125cb3e.chunk.js"
+  },
+  {
+    "revision": "dd9adfdb09f83cb1a7aa",
+    "url": "/todo/static/js/runtime-main.f7a45fe9.js"
+  }
+]);
